5f5a8180466fabbaf1fe213fa1a8a9d066e9967a
Hollow666 <53885597+Hollow666@users.noreply.github.com>
2020-03-27 20:26:05 +0300

    25.03 | Прикладное программирование | ДОМИКgit add -A!

---
+++ 27.03/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace домик
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // box
+            // 
+            this.box.BackColor = System.Drawing.SystemColors.Control;
+            this.box.Location = new System.Drawing.Point(1, -1);
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(800, 450);
+            this.box.TabIndex = 0;
+            this.box.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.box);
+            this.MaximumSize = new System.Drawing.Size(600, 600);
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "Form1";
+            this.Text = "ДОМИК!!!!!!!!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.box)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox box;
+    }
+}
+
---
+++ 27.03/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace домик
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            box.Width = 600;
+            box.Height = 600;
+            Bitmap bitmap = new Bitmap(box.Width, box.Height);
+            Graphics img = Graphics.FromImage(bitmap);
+            img.Clear(System.Drawing.Color.Blue);
+            
+            Point[] points = new Point[3] {new Point(150, 200), new Point(450, 200), new Point(300, 50)};
+            
+            //img.DrawLine(myPen, 150, 200, 300, 50);
+            //img.DrawLine(myPen, 300, 50, 450, 200);
+
+
+            SolidBrush brush = new SolidBrush(System.Drawing.Color.Red);
+            img.FillRectangle(brush, new Rectangle(150, 200, 300, 250));
+            brush.Color = System.Drawing.Color.Green;
+            img.FillRectangle(brush, new Rectangle(250, 300, 100, 150));
+            brush.Color = System.Drawing.Color.Yellow;
+            img.FillPolygon(brush, points);
+            brush.Color = System.Drawing.Color.LightBlue;
+            img.FillEllipse(brush, new Rectangle(250, 90, 100, 100));
+
+            System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Black);
+            img.DrawRectangle(myPen, new Rectangle(150, 200, 300, 250));
+            img.DrawRectangle(myPen, new Rectangle(250, 300, 100, 150));
+            img.DrawEllipse(myPen, new Rectangle(250, 90, 100, 100));
+            img.DrawPolygon(myPen, points);
+
+            myPen.Dispose();
+            brush.Dispose();
+            img.Dispose();
+            box.Image = bitmap;
+        }
+    }
+}
